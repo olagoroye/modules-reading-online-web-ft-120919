37552c7298f53ed6d1442d670aa9ab2a0ba180fd
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-06 10:21:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,18 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 
-class Dancer
-   include Dance
- extend MetaDancing
-  attr_accessor :name
+# class Dancer
+#   include Dance
+# extend MetaDancing
+#   attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
 
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,15 +1,19 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
+# class Kid
   
-   include Dance
-   extend MetaDancing
-  attr_accessor :name
+#   include Dance
+#   extend MetaDancing
+#   attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
-
-
